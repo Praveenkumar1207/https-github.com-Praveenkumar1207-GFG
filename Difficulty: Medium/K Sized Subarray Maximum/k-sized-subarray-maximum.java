9499d3793619e91b7263801c1872017e1ac9fff8
V PRAVEEN KUMAR <119840051+Praveenkumar1207@users.noreply.github.com>
2024-09-27 22:23:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial template for JAVA
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // taking input using class Scanner
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            ArrayList<Integer> res = new Solution().max_of_subarrays(k, arr);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for JAVA
+
+class Solution {
+    // Function to find maximum of each subarray of size k.
+    public int solve(int i,int j,int[] arr){
+        int res = Integer.MIN_VALUE;
+        while(i <= j){
+            res = Math.max(arr[i],res);
+            i++;
+        }
+        return res;
+    }
+    public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
+        // Your code here
+        ArrayList<Integer> res = new ArrayList<>();
+        int prev = solve(0,k - 1,arr);
+        res.add(prev);
+        // int prev = Integer.MIN_VALUE;
+        for(int i = 1;i < arr.length - k + 1; i++){
+            if(arr[i - 1] == prev){
+                prev = solve(i,i + k - 1,arr);
+                res.add(prev);
+            }
+            else{
+                prev = Math.max(prev,arr[i + k - 1]);
+                res.add(prev);
+            }
+        }
+        return res;
+    }
+}
