48f21bc2630a796d24d39c7b83d48046535638f5
V PRAVEEN KUMAR <119840051+Praveenkumar1207@users.noreply.github.com>
2024-11-05 22:23:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[][] arr = new int[n][n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) arr[i][j] = sc.nextInt();
+
+            GFG g = new GFG();
+            g.rotate(arr);
+            printMatrix(arr);
+
+            System.out.println("~");
+        }
+    }
+
+    static void printMatrix(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) System.out.print(arr[i][j] + " ");
+            System.out.println("");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class GFG {
+    static void rotate(int mat[][]) {
+         int n = mat.length;
+        
+        //transpose the matrix
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        
+        //reverse the array
+        for(int i=0;i<n;i++){
+            int start = 0, end = n-1;
+            while(start<end){
+                int temp = mat[i][start];
+                mat[i][start] = mat[i][end];
+                mat[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
