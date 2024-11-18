7395cfff6fbc4b8a94e1e33f422e20dcaf4c2aad
V PRAVEEN KUMAR <119840051+Praveenkumar1207@users.noreply.github.com>
2024-11-18 21:13:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+         int n = arr.length;
+        d = d % n;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<d; i++) {
+            list.add(arr[i]);
+        }
+        
+        int i = 0;
+        for(i=0; i<n-d; i++) {
+            arr[i] = arr[i+d];
+        }
+        
+        for(int j=0; j<list.size(); j++) {
+            arr[i] = list.get(j);
+            i++;
+        }
+    }
+}
