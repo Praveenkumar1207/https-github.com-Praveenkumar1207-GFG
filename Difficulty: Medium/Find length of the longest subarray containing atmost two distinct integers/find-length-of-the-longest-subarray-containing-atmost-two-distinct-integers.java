bd957f1423f04b619a3a3c2f2bebdf0b01114b3f
V PRAVEEN KUMAR <119840051+Praveenkumar1207@users.noreply.github.com>
2025-01-21 21:04:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find length of the longest subarray containing atmost two distinct integers/find-length-of-the-longest-subarray-containing-atmost-two-distinct-integers.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            // Convert ArrayList to array
+            Integer[] arr = new Integer[array.size()];
+            array.toArray(arr);
+
+            Solution ob = new Solution();
+
+            // Call totalFruits method and print result
+            Integer ans = ob.totalElements(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int totalElements(Integer[] arr) {
+        // code her
+        int i = 0;
+        int j = 0;
+        int ans = -1;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        while(i < arr.length ){
+            map.put(arr[i],map.getOrDefault(arr[i],0) + 1);
+            if(map.size() <= 2){
+                ans = Math.max(i - j + 1,ans);
+            }
+            while(map.size() > 2){
+                map.put(arr[j],map.getOrDefault(arr[j],0) - 1);
+                if(map.get(arr[j]) <= 0){
+                    map.remove(arr[j]);
+                }
+                j++;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
